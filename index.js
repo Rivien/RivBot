b2e36f821480f631271d71617b9227178e6e8dde
Rivien <39446589+Rivien@users.noreply.github.com>
2019-02-05 23:10:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
   // Example of changing the bot's playing game to something useful. `client.user` is what the
   // docs refer to as the "ClientUser".
-  client.user.setActivity(`r!help | I am on ${client.guilds.size} servers <3`);
+  client.user.setActivity(`r!help | I am on ${client.guilds.size} servers :D`);
 });
 
 client.on("guildCreate", guild => {
